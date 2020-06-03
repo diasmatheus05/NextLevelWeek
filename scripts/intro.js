ce1826a67b1951eb9e96000630088c3a81b46718
Matheus Dias <diasmatheus05@gmail.com>
2020-06-03 00:27:16 -0300

    Pagina de cadastro de coleta, primeira parte

---
+++ scripts/intro.js
@@ -0,0 +1,60 @@
+// comments
+//document.write("Hello")
+
+// variaveis, tipos de dados
+//var myvar = "Hello" // let 
+//const myconst = "he"
+
+// string 
+//const s1 = "String",
+//const s2 = 'String',
+
+// number
+//const n1 = 1
+//const n2 = 12
+
+// boolean - true ou false
+//const bTrue = True
+//const bFalse = false
+
+// objeto - possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr() {
+//     return "run forest"
+//   }
+// }
+
+//document.write(pessoa.altura)
+//document.write(pessoa.idade)
+//document.write(pessoa.correr())
+
+// array
+// const myarray = ["blue", "green", 1, {name: "My name"}]
+
+// document.write(myarray)
+// document.write(myarray[0])
+// document.write(myarray[3].name)
+
+// função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// sayMyName("Matheus")
+
+// condicionais
+
+// if ( true ) {
+//
+// } else {
+//
+// }
+
+// loop
+
+// for ( ; ; ) {
+//
+// }
